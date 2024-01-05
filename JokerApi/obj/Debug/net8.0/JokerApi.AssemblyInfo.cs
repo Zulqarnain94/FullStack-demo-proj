43529ea605e88294b97e94df5659a6406edd3ab9
Muhammad Zulqarnain Aslam <147504664+Zulqarnain94@users.noreply.github.com>
2024-01-05 20:38:28 +0100

    Style

--- JokerApi/obj/Debug/net8.0/JokerApi.AssemblyInfo.cs
+++ JokerApi/obj/Debug/net8.0/JokerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JokerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2999783f6adfc667e6c38c86033fd0ba99e0c6d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("JokerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JokerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- next-app/app/zulqi/fetchCom.tsx
+++ next-app/app/zulqi/fetchCom.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Result } from "postcss";
 import React, { useState, useEffect } from "react";
 
 const FetchCom = () => {
@@ -15,8 +16,25 @@ const FetchCom = () => {
   useEffect(() => {
     fetchData();
   }, []);
+  console.log({ data });
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div className="weather-forecast">
+      <h1>Weather Forecast</h1>
+      {data && (
+        <ul className="forecast-list">
+          {data.map((item, index) => (
+            <li key={index} className="forecast-item">
+              <strong>Date:</strong> {item.date}, &nbsp;
+              <strong>Temperature (C):</strong> {item.temperatureC}, &nbsp;
+              <strong>Summary:</strong> {item.summary}, &nbsp;
+              <strong>Temperature (F):</strong> {item.temperatureF}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default FetchCom;
